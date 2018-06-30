aef000b04227eaf3e490cddfeb4efbb08cc58df0
AndriyOda <34584060+AndriyOda@users.noreply.github.com>
2018-06-30 19:41:26 +0300

    PHP script condig

--- ru/FeedBack/submit.php
+++ ru/FeedBack/submit.php
@@ -1,20 +1,38 @@
 <?php
-if (!empty($_POST['name']) AND !empty($_POST['email'] ) {
-	
-	$headers = 'From: Andriy Oda';
-	$theme   = 'New messeng'
+  //Если форма отправлена
+  if(isset($_POST['submit'])) {
+ //Проверка Поля ИМЯ
+  if(trim($_POST['name']) == '') {
+  $hasError = true;
+  } else {
+  $name = trim($_POST['name']);
+  }
 
-	$letter  ='Name:'.$_POST['name'].'\r\n';
-	$letter .='Email:'.$_POST['email'].'\r\n';
-	$letter .='Email:'.$_POST['messeng'].'\r\n';
-
-
-	if (mail('kolyaodri.ko@gmail.com', $theme, $letter)){
-      header("Location: /ToOrder/thankyou.html");
-    } else {
-      header("Location: /ToOrder/error.html");
-    }  
-              
-} else {
-  header("Location: /ToOrder/no-s.html");
-}
+ //Проверка правильности ввода EMAIL
+  if(trim($_POST['email']) == '')  {
+  $hasError = true;
+  } else if (!eregi("^[A-Z0-9._%-]+@[A-Z0-9._%-]+\.[A-Z]{2,4}$", trim($_POST['email']))) {
+  $hasError = true;
+  } else {
+  $email = trim($_POST['email']);
+  }
+ //Проверка наличия ТЕКСТА сообщения
+  if(trim($_POST['message']) == '') {
+  $hasError = true;
+  } else {
+  if(function_exists('stripslashes')) {
+  $comments = stripslashes(trim($_POST['message']));
+  } else {
+  $comments = trim($_POST['message']);
+  }
+  }
+ //Если ошибок нет, отправить email
+  if(!isset($hasError)) {
+  $emailTo = 'kolyaodri.ko@gmail.com'; //Сюда введите Ваш email
+  $body = "Name: $name \n\nEmail: $email \n\nSubject: $subject \n\nComments:\n $comments";
+  $headers = 'From: My Site <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+ mail($emailTo, $subject, $body, $headers);
+  $emailSent = true;
+  }
+  }
+  ?>
