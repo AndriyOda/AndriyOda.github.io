119a7e058f385b13c1bc642a78fd00d329b17317
AndriyOda <34584060+AndriyOda@users.noreply.github.com>
2018-07-01 20:12:21 +0300

    PHP&JS

---
+++ en/FeedBack/mail.php
@@ -0,0 +1,12 @@
+<?php
+
+$recepient = "agragregra@ya.ru";
+$sitename = "Название сайта";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["email"]);
+$text = trim($_POST["message"]);
+$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
